93fb87b4346ce86509ec08c1e02afc026a322c83
jasper <jasperzhou1129@gmail.com>
2021-05-27 23:50:45 -0400

    micro demo

---
+++ MacrosDemo/MacrosDemo/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  MacrosDemo
+//
+//  Created by Jinglun Zhou on 2021/5/27.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define micro cout << "preprocessor" << endl;
+
+int main(int argc, const char * argv[]) {
+    micro;
+
+}
